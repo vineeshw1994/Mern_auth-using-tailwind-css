826948d161869ea25ae77f81b5fdc2712e0df4a7
vineesh <vineeshflorida@gmail.com>
2023-12-11 13:23:26 +0530

    add a middleware and a finction to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from '../models/user.model.js'
 import bcrypt, { hashSync } from 'bcrypt';
+import {errorHandler} from '../utils/error.js'
 
-export const signup = async(req,res)=>{
+export const signup = async(req,res,next)=>{
     const {username,email,password}=req.body;
     const hashedPassword =  hashSync(password, 10) 
     const newUser = new User({username,email,password:hashedPassword})
@@ -9,6 +10,6 @@ export const signup = async(req,res)=>{
         await newUser.save()
         res.status(201).json({message:'user created successfully'})
      }catch(error){
-        res.status(500).json(error.message)
+        next(error)
      }
 }
--- api/index.js
+++ api/index.js
@@ -19,6 +19,16 @@ app.use(express.json());
 
 app.use('/api/user', userRoutes)
 app.use('/api/auth',authRoutes)
+
+app.use((err,req,res,next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    error:message,
+    statusCode:statusCode,
+  })
+})
  
 
 app.listen(3000, () => {

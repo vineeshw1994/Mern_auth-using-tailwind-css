33f40b93f30777da67153d553a10f5006fa63474
vineesh <vineeshflorida@gmail.com>
2023-12-12 00:09:48 +0530

    create in my sign in route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcrypt, { hashSync } from 'bcrypt';
 import {errorHandler} from '../utils/error.js'
+import jwt from 'jsonwebtoken'
 
 export const signup = async(req,res,next)=>{
     const {username,email,password}=req.body;
@@ -12,4 +13,23 @@ export const signup = async(req,res,next)=>{
      }catch(error){
         next(error)
      }
+}
+
+export const signin = async(req,res,next)=>{
+   const {email,password}=req.body;
+   try{
+      const validUser = await User.findOne({email})
+      if(!validUser) return next(errorHandler(401, ' Invalid credetials'))
+
+      const validPassword = bcrypt.compareSync(password, validUser.password)
+      if(!validUser) return next(errorHandler(401, ' Invalid credetials'))
+      const token = jwt.sign({_id:validUser._id},process.env.JWT_SECRET)
+   const {password: hashedPassword, ...rest} = validPassword._doc
+   const expiryDate = new Date(Date.now() + 3600000)
+      res.cookie('access_token', token, {httpOnly: true,},expiryDate)
+      .status(200)
+      .json(rest)
+   }catch(error){
+      next(error)
+   }
 }

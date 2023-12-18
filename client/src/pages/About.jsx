43984b7a4998b1e7f598baccd928a5ddd3591480
vineesh <vineeshflorida@gmail.com>
2023-12-18 12:28:01 +0530

    complere home and about pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,10 @@
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-700">Welcome to unity link</h1>
+      <p className="mb-4 text-slate-400">This is our social media platform....</p>
+    </div>
   )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,10 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-700">Welcome to unity link</h1>
+      <p className="mb-4 text-slate-400">This is  a full-stack web application built with the MERN (mongodb, express, react, node.js) stack. It inclueds authentication features that allow users to sign up, log in and log out , and provides access to protected routes only for authenticated users.</p>
+    </div>
   )
 }
 

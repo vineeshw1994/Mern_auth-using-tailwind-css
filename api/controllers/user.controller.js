2c9374dcc8aaeaea629295a57285da305889972f
vineesh <vineeshflorida@gmail.com>
2023-12-18 10:16:32 +0530

    ceate update user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,34 @@
+import {errorHandler} from '../utils/error.js'
+import User from '../models/user.model.js'
+import bcrypt from 'bcrypt'
 export const test = (req,res)=> {
     res.json({
         message:'hello vineesh'
     })
+}
+
+export const updateUser = async(req,res,next)=>{
+    if(req.user.id !== req.params.id){
+        return res.status(401).json('you can update only your account')
+        return next(errorHandler(401,'You can update only your account'))
+    }
+
+    try{
+        if(req.body.password){
+            req.body.password = bcrypt.hashSync(req.body.password, 10)
+        }
+        const updateUser = await User.findByIdAndUpdate(
+            req.params.id,{$set:{
+                username:req.body.username,
+                email:req.body.email,
+                password:req.body.password,
+                profilePic:req.body.profilePic
+            }},
+            {new:true}
+        );
+        const {password,...rest} =updateUser._doc;
+        res.status(200).json(rest)
+    }catch(error){
+        console.log(error.message)
+    }
 }
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js'
@@ -21,6 +22,7 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
+app.use(cookieParser());
  
 
 app.use('/api/user', userRoutes)
